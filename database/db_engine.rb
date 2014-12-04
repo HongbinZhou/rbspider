ee9278fba928dd02fabf42b6705b543ae91a3430
Minghe Huang <h.minghe@gmail.com>
2014-12-04 23:14:50 +0800

    enable the two type data format of table column

--- database/db_engine.rb
+++ database/db_engine.rb
@@ -4,7 +4,7 @@ module DatabaseEngine
   def self.save(object)
     if object.respond_to?("database_to_save") && object.respond_to?("table_to_save")
       if !table_exists?(object.database_to_save, object.table_to_save + 's')
-        setup_table(object.database_to_save, object.table_to_save + 's', object.keys)
+        setup_table(object.database_to_save, object.table_to_save + 's', object)
       end
       begin
         Kernel.const_get(object.table_to_save.capitalize + 's').new(object).save!
@@ -21,20 +21,23 @@ module DatabaseEngine
   end
 
   private
-
   def self.table_exists?(db_name, table_name)
     if connect_db(db_name)
       ActiveRecord::Base.connection.table_exists? "#{table_name}"
     end
   end
 
-  def self.setup_table(db_name, table_name, columns)
-    if db_name && table_name && columns.length > 0
+  def self.setup_table(db_name, table_name, object)
+    if db_name && table_name && object.keys.length > 0
       if connect_db(db_name)
         ActiveRecord::Schema.define do
           create_table :"#{table_name}" do |table|
-            columns.each do |column_name|
-              table.column "#{column_name}", "text"
+            object.keys.each do |column_name|
+              if object[:column_name].is_a?(DateTime)
+                table.column "#{column_name}", "datetime"
+              else
+                table.column "#{column_name}", "text"
+              end
             end
           end
         end
